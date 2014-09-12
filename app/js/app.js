0d600f4c5f16f3551f3f8ec4cbad39c0dca1210f
Anupam Singh <anupam.singh@valuelabs.net>
2014-09-12 19:57:18 +0530

    Startup: included oauth-js

--- app/js/app.js
+++ app/js/app.js
@@ -5,3 +5,18 @@ app.constant('_', _);
 app.controller('appCtrl', ['$scope', function($scope){
   console.log('App controller');
 }]);
+
+
+OAuth.initialize('YOURPUBLICKEY');
+
+  
+OAuth.popup('twitter')
+.done(function(result) {
+  //use result.access_token in your API request 
+  //or use result.get|post|put|del|patch|me methods (see below)
+  console.log(result);
+})
+.fail(function (err) {
+  //handle error with err
+  console.log(err);
+});
